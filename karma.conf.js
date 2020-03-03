77247d87fb96b44216274f9b6faf64c4aca9024e
GerardoRodvel <jose_gerardo_lpz@hotmail.com>
2020-03-03 16:45:35 -0600

    ayuda diosito

--- karma.conf.js
+++ karma.conf.js
@@ -25,14 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+     browsers: ['Chrome'],
     customLaunchers: {
-      ChromeNoSandbox: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
+        ChromeHeadlessCI: {
+            base: 'ChromeHeadless',
+            flags: ['--no-sandbox']
+        }
     },
-    
     singleRun: false,
     restartOnFileChange: true
   });
